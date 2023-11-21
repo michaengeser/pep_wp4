5c2f877e3a0a420ea44b7e15e718df80260b3a83
michaengeser <125463198+michaengeser@users.noreply.github.com>
2023-11-21 19:47:07 +0100

    fixing saving

---
+++ data/sub-114/ses-2/familiarity/codebathroom/getInput.m
@@ -0,0 +1,25 @@
+
+% GETINPUT a single iteration of input seeking, returns the RT and address of key pressed
+% Input:
+% -------
+% key - the identity of the key pressed by the user
+% Resp_Time - the RT of the user's key press
+% PauseTime - If the pause key is pressed, the duration of the pause is
+% recorded to make sure we account for it when starting again!
+function [ key, Resp_Time ] = getInput()
+
+global NO_KEY 
+
+%%
+key = NO_KEY;
+
+[KeyIsDown, Resp_Time, Resp1] = KbCheck();
+
+if KeyIsDown
+key = find(Resp1);
+end
+
+if key == NO_KEY
+    Resp_Time = [];
+end
+end
