678d535adfbbc06e6e90a72ea71433f86041f005
michaengeser <125463198+michaengeser@users.noreply.github.com>
2024-01-09 13:53:09 +0100

    make MDS

--- untitled3.m
+++ untitled3.m
@@ -8,11 +8,10 @@ userOptions.analysisName = 'Micha';
 RDM.RDM = RDM.RDM + 1;
 RDM.RDM(1:size(RDM.RDM, 1)+1:end) = 0;
 
+dotColours = repmat([1,0,0],length(RDMs),1);
 
 % 2nd order MDS
-rsa.MDSRDMs({RDMs}, userOptions, struct('titleString', 'MDS of different RDMs', 'figureNumber', 13,'fileName','2ndOrderMDSplot'));
-
-
+rsa.MDSRDMs({RDMs}, userOptions, struct('titleString', 'MDS of different RDMs', 'figureNumber', 13,'fileName','2ndOrderMDSplot', 'dotColours',dotColours));
 
 rsa.MDSConditions(RDM, userOptions,struct('titleString','ground-truth MDS',...
-    'fileName','trueRDM_MDS','figureNumber',6));
+    'fileName','trueRDM_MDS','figureNumber',6,'dotColours',dotColours, 'alternativeConditionLabels', string(d.subNums)));
