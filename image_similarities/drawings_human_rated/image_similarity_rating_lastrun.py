b132b49458f8cdbb3705ad9649b02324013617dc
michaengeser <125463198+michaengeser@users.noreply.github.com>
2024-03-22 13:55:31 +0100

    new subject

--- image_similarities/drawings_human_rated/image_similarity_rating_lastrun.py
+++ image_similarities/drawings_human_rated/image_similarity_rating_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.2.3),
-    on Thu Mar 21 11:41:56 2024
+    on Fri Mar 22 12:25:57 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
